eb0e924d6039312c325d039e0636edb2f13b7646
Marium Jawaid <162020287+MariumJawaid@users.noreply.github.com>
2025-09-14 23:17:44 +0500

    Add files via upload

---
+++ basics_01.js
@@ -0,0 +1,15 @@
+// const account = 12332
+// const accname = "moosa"
+// const password = "mamam"
+
+// console.table([account,accname,password])
+
+
+const mar = function() {
+    let user = "marium"
+    console.log(this.user)
+}
+
+mar()
+
+// console.log(this)
